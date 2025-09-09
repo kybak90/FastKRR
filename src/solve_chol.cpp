14409544c184427c17e23a136528fdb5d9d93fa0
kybak90 <kybak@sungshin.ac.kr>
2025-09-09 13:40:02 +0900

    get_num_proc

--- src/solve_chol.cpp
+++ src/solve_chol.cpp
@@ -8,6 +8,16 @@ using namespace Rcpp;
 using namespace arma;
 
 // [[Rcpp::export]]
+int get_num_procs() {
+  int max_threads = 1;
+  #ifdef _OPENMP
+    max_threads = omp_get_num_procs();
+  #endif
+  return max_threads();
+}
+
+
+// [[Rcpp::export]]
 arma::vec solve_chol(const arma::mat& A,
                      const arma::vec& b)
 {
