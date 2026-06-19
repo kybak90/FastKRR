ecb5f613994c990ab1ac7a9dcbf66a5b4296edbf
unknown <you@example.com>
2026-06-19 12:41:51 +0900

    attr

--- R/CVST_linkfunction.R
+++ R/CVST_linkfunction.R
@@ -405,17 +405,17 @@ fastkrr = function(x, y,
     K = make_kernel(x, kernel = kernel, rho = rho, n_threads = n_threads)
     rslt = nystrom(K, m = m, y, lambda = lambda, n_threads = n_threads)
 
-    attr(result_values, "coefficients") = rslt$coefficients
-    attr(result_values, "fitted.values") = as.vector(K %*% rslt$coefficients)
-    attr(result_values, "opt") = opt
-    attr(result_values, "kernel") = kernel
-    attr(result_values, "x") = x
-    attr(result_values, "y") = y
-    attr(result_values, "lambda") = lambda
-    attr(result_values, "rho") = rho
-    attr(result_values, "n_threads") = n_threads
-    attr(result_values, "fastcv") = fastcv
-    attr(result_values, "call") = call
+    result_values$coefficients = rslt$coefficients
+    result_values$fitted.values = as.vector(K %*% rslt$coefficients)
+    result_values$opt = opt
+    result_values$kernel = kernel
+    result_values$x = x
+    result_values$y = y
+    result_values$lambda = lambda
+    result_values$rho = rho
+    result_values$n_threads = n_threads
+    result_values$fastcv = fastcv
+    result_values$call = call
 
     attr(result_values, "K_approx") = tcrossprod(rslt$R)
     class(attr(result_values, "K_approx")) = "kernel_matrix"
